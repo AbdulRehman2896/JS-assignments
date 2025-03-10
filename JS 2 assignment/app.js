7bd38e353682493ec6a8137dd968e3b109fea893
Abdul Rehman <abdulrehman089788@gmail.com>
2025-03-10 14:33:44 +0500

    Add files via upload

---
+++ JS 2 assignment/app.js
@@ -0,0 +1,86 @@
+//alert("enter your password");
+//alert("welcome to js land ");
+//console.log("hello i can run js");
+//var name1 = Abdul;
+//var name2 = Rehman;
+//var add = name1 + name2;
+
+
+//var name = prompt(" what is your name")
+//var birthyear = prompt("your birthyear");
+      //legal variables
+//var $fant = good;
+//var name = xxx;
+    // illegal variables
+
+//var you & 1 = ttt;
+//var card  htt = bbb;
+
+//var num1 = 5;
+//var num2 = 6; 
+//var total = num1 + num2;
+//var i = 5; i++;
+//console.log("i");
+//var birthyear = prompt("your age year");
+//var currentyear = 2025;
+//var age = currentyear - birthyear;
+//
+//var num = [20,30,40,10]
+// num.splice(0,2)
+//console.log(num)
+/*
+var sum = 0;
+console.log(sum)
+for (a=0;a<num.length;a++){
+   // console.log(num[a])
+    sum = sum+num[a] 
+}
+console.log(sum)
+var student = {
+    name : "Ali",
+    class : 5,
+}
+console.log(student)
+
+var tot = [{
+    name : ["Ali","ddd","ttt"],
+    vaggj : [{ggwggsg:gggg,}],
+    class : 5,},
+{
+    name : "Ali",
+    class : 5,},
+    {
+    name : "Ali",
+    class : 5}]
+console.log(tot[0].name[2])
+var a = 2
+if (a=2)
+{console.log("true")}
+else {
+    console.log("false")
+}
+
+var t = 3
+var d = [t+4,t+2,t+1]
+t = d.length-1
+console.log(t)
+var t = prompt("what is your name")
+alert("your name is  "+ t)
+
+var product = {
+    name : "ali",
+    calss : 6,
+    subject : "physics",
+    othersubj : ["math","comp","eng","urdu"]
+    
+}
+console.log(product.othersubj[3])
+var sum1 = [3,4,5,6,7,9];
+sum1.splice(2,4,)
+console.log(sum1)
+var sum = 0;
+for (i=0; i < sum1.length; i++){
+    sum = sum + sum1[i]
+    
+}
+console.log(sum)*/

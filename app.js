c2079564aa4e5658ada94716ef2254d2656962d0
Abdul Rehman <abdulrehman089788@gmail.com>
2025-03-10 13:48:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+//var num1 = +prompt()
+/*var age = +prompt("write your age")
+if (age >18){
+   console.log("you are eligible for vote")
+}
+else {
+    console.log("you are not eligible for vote")
+}
+var num1 = +prompt("write number")
+var num2 = +prompt("write number")
+
+if (num1 > num2){
+   console.log("largest number is  " + num1)
+}
+else if (num1 < num2){
+    console.log("largest number is  " + num2)
+}
+var age = +prompt("write your age")
+var card = prompt("have you card?ture/false ")
+console.log(card)
+
+if (age > 60 || card == true){
+   console.log("eligible for a discount")
+}
+else{
+    console.log("not eligible for a discount")
+    
+}
+
+var user = "admin"
+var pass = 12345
+var username = prompt("type username")
+var password = +prompt("type password")
+
+if (user === username && pass === password){
+    console.log("login successful")
+}
+else {
+    console.log("Invalid credentials")
+}
+var grade =  +prompt("type your marks")
+
+if (100 > grade && grade > 90){
+    console.log("you got A grade")
+}
+else if ( 90 > grade  && grade > 80){
+    console.log("you got B grade")
+}
+else if (80 > grade  && grade > 70){
+    console.log("you got C grade")
+}
+else if (70 > grade  && grade > 60){
+    console.log("you got D grade")
+}
+else if (grade < 60){
+    console.log("you got F grade")
+}
+else{
+    console.log("invalid number")
+    
+}*/
+var color = prompt("write traffic colour")
+if (color === "red"){
+   console.log("Stop")
+}
+else if (color === "green"){
+    console.log("Go")
+}
+else if (color === "yellow"){
+    console.log("Slow down")
+}
+else{
+    console.log("incorrect")
+}

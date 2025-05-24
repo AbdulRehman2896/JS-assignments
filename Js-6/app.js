169736e786ddb3bfed3da7f7e431b14b8c0d545b
Abdul Rehman <abdulrehman089788@gmail.com>
2025-05-24 19:00:06 +0500

    Add files via upload

---
+++ Js-6/app.js
@@ -0,0 +1,18 @@
+
+/*
+var name = prompt("write your name")
+
+var DOM = document.getElementById("id1")
+ DOM.innerText = "Hello  " + name
+console.log(DOM)*/
+
+
+var doc = document.getElementsByTagName("body")
+console.log(Math.round(Math.random()*265))
+function  color(){
+   
+    doc[0].style.backgroundColor = 
+   "rgb(Math.round(Math.random())*256 +" "+ Math.round(Math.random())*256 + " " + Math.round(Math.random())*265"
+    
+}
+

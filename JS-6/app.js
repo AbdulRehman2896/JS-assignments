9caae0b857c4a36394787eefdac223f5dc9974ba
Abdul Rehman <abdulrehman089788@gmail.com>
2025-05-30 17:04:12 +0500

    Add files via upload

---
+++ JS-6/app.js
@@ -0,0 +1,28 @@
+var list = document.querySelector("#lists")
+
+var listbtn = document.querySelector("btn")
+
+console.log(list)
+btn.addEventListener('click',listing())
+function listing(){
+    
+
+var listquan = list.children.length + 1
+lists.innerHTML += `<li>Task-${listquan }</li>`
+    updateTaskClickEvents()
+}
+function updateTaskClickEvents(){
+    const current = document.querySelectorAll('#btn li')
+}
+
+
+
+
+
+
+
+
+
+
+
+
